0f11c61532fe2b2955d46f5af31bc5788fa64856
P. PRANEETH REDDY <praneethr1000@gmail.com>
2018-12-30 21:06:14 +0530

    Add files via upload

---
+++ advancedWordCloud.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 30 20:31:50 2018
+
+@author: praneeth
+"""
+
+from os import path
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from wordcloud import WordCloud, STOPWORDS
+
+# get data directory (using getcwd() is needed to support running example in generated IPython notebook)
+d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+
+# Read the whole text.
+text = open(path.join(d, 'comments.txt')).read()
+
+# read the mask image
+alice_mask = np.array(Image.open(path.join(d, "alice_mask.png")))
+
+stopwords = set(STOPWORDS)
+stopwords.add("said")
+
+wc = WordCloud(background_color="white", max_words=2000, mask=alice_mask,
+               stopwords=stopwords, contour_width=3, contour_color='steelblue')
+
+# generate word cloud
+wc.generate(text)
+
+# store to file
+wc.to_file(path.join(d, "sample5.png"))
+
+# show
+plt.imshow(wc, interpolation='bilinear')
+plt.axis("off")
+plt.show()
